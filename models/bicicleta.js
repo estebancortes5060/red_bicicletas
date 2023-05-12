f69ec09ee872ad1903cebbd775194de940d52468
estebancortes5060 <juecortesr@udistrital.edu.co>
2023-05-11 19:30:45 -0500

    FinalCRUD

---
+++ models/bicicleta.js
@@ -0,0 +1,42 @@
+var Bicicleta = function (id, color, modelo, ubicacion) {
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+Bicicleta.prototype.toString = function () {
+    return 'id: ' + this.id + " | color: " + this.color;
+}
+
+Bicicleta.allBicis = [];
+Bicicleta.add = function(aBici){
+    Bicicleta.allBicis.push(aBici);
+}
+
+Bicicleta.findById = function (aBiciId) {
+    var aBici = Bicicleta.allBicis.find(x => x.id == aBiciId);
+    if (aBici){
+        return aBici;
+    } else {
+        throw new Error(`No existe una bicicleta con el id ${aBiciId}`)
+    }
+}
+
+Bicicleta.removeById = function (aBiciId) {
+    //var aBici = Bicicleta.findById(aBiciId);
+    for (var i = 0; i < Bicicleta.allBicis.length; i++) {
+        if (Bicicleta.allBicis[i].id == aBiciId) {
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }        
+    }
+}
+
+var a = new Bicicleta(1, 'Rojo', 'Urbana', [4.580278, -74.158046]);
+var b = new Bicicleta(2, 'Blanca', 'Urbana', [4.578935, -74.156734]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
